daac043241c370d61cf1ee42877a249d1c2cdc21
Geo <gravyplaya@gmail.com>
2023-11-03 00:15:35 -0700

    featured section update

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -212,20 +212,20 @@ export const LandingContent = () => {
           {bots.map((item) => (
             <div
               key={item.description}
-              className="block rounded-lg bg-white  shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+              className="block rounded-lg bodyGradient text-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
             >
-              <div className="border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50 text-center font-bold">
+              <div className="border-b-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50 text-center font-bold text-xl">
                 Featured
               </div>
               <div className="p-6">
-                <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 text-center break-normal">
+                <h5 className="mb-2 text-xl font-medium leading-tight text-white dark:text-neutral-50 text-center break-normal">
                   {item.name}
                 </h5>
-                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                <p className="mb-4 text-base text-white dark:text-neutral-200">
                   {item.description}
                 </p>
                 <div className="border-t-2 border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-                  <div className="text-sm text-neutral-600 dark:text-neutral-200">
+                  <div className="text-sm text-white dark:text-neutral-200">
                     <p>Features</p>
                     <ol className="list-decimal list-inside">
                       {item.features?.map((feature) => (
